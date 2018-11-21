370ef418ac23453ce04ab510048bc95275540b75
Jefferson Alves <jefferson.alves.costa@gmail.com>
2018-11-21 00:53:02 -0300

    Funciona

---
+++ sol/presenca.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.23;
+
+contract Presenca{
+    struct Aluno{
+        bool matriculado;
+        uint presencas;
+    }
+
+    address public professor;
+    mapping(address => Aluno) public turma;
+
+
+    function Presenca() public{
+        professor = msg.sender;
+    }
+
+    function registraAluno(address aluno) public{
+        if (msg.sender != professor) return;
+        turma[aluno].matriculado = true;
+        turma[aluno].presencas = 0;
+    }
+
+    function assinaPresenca() public{
+        // Aluno assina a lista de presenca
+        if (msg.sender == professor || turma[msg.sender].matriculado == false) return;
+        turma[msg.sender].presencas += 1;
+    }
+
+}
